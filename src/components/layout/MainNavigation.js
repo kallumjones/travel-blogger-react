23fe713b3f7c45bd0c7a0d2450a3f8382b2dfe17
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-10-12 11:47:00 +0100

    Add Navigation and Styling

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+//import CSS file
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>Travel Blogger</div>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link to="/">All Travel Blogs</Link>
+                    </li>
+                    <li>
+                        <Link to="/new-travel-blog">Create New Travel Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/bucket-list">Bucket List</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
