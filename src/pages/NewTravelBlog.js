2ad6ae1a3fe77f0ef88af9e32b7b050f4efd8168
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-10-12 14:59:30 +0100

    Add form

--- src/pages/NewTravelBlog.js
+++ src/pages/NewTravelBlog.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import NewTravelBlogForm from '../components/blogs/NewTravelBlogForm'
 
 const NewTravelBlog = () => {
     return (
         <div>
             <h1>New Travel Blog</h1>
-
-            
+            <NewTravelBlogForm />
         </div>
     )
 }
