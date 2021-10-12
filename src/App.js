31633a5a68e279b0d64a56d33de1575c9ce746b9
Kallum Jones <kallum.jones@autotrader.co.uk>
2021-10-12 11:24:23 +0100

    Add initial components, add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+// bring in pages:
+import AllTravelBlogs from "./pages/AllTravelBlogs";
+import NewTravelBlog from "./pages/NewTravelBlog";
+import BucketList from "./pages/BucketList";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllTravelBlogs />
+        </Route>
+
+        <Route path="/new-travel-blog">
+          <NewTravelBlog />
+        </Route>
+
+        <Route path="/bucket-list">
+          <BucketList />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/BucketList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BucketList = () => {
+    return (
+        <div>
+            <h1>My bucket List</h1>
+        </div>
+    )
+}
+
+export default BucketList
